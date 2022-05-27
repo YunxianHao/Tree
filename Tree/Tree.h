60fd960f76fd1c6906bcfbfd7b1483444d0e002d
YunxianHao <92800800+YunxianHao@users.noreply.github.com>
2022-05-27 22:35:34 +0800

    Create Tree.h

---
+++ Tree/Tree.h
@@ -0,0 +1,31 @@
+typedef char BTDataType;
+ 
+typedef struct BinaryTreeNode
+{
+	BTDataType _data;
+	struct BinaryTreeNode* _left;
+	struct BinaryTreeNode* _right;
+}BTNode;
+ 
+//The binary tree is constructed through the array "ABD##E#H##CF##G##"
+BTNode* BinaryTreeCreate(BTDataType* a, int n, int* pi);
+// Binary tree destruction
+void BinaryTreeDestory(BTNode** root);
+//Number of binary tree nodes
+int BinaryTreeSize(BTNode* root);
+// Number of leaf nodes of binary tree
+int BinaryTreeLeafSize(BTNode* root);
+// Number of nodes in the k-th layer of binary tree
+int BinaryTreeLevelKSize(BTNode* root, int k);
+// Binary tree finds nodes with value x
+BTNode* BinaryTreeFind(BTNode* root, BTDataType x);
+// PreOrder 
+void BinaryTreePrevOrder(BTNode* root);
+// InOreder
+void BinaryTreeInOrder(BTNode* root);
+// PostOrder
+void BinaryTreePostOrder(BTNode* root);
+// level traversal
+void BinaryTreeLevelOrder(BTNode* root);
+// Judge whether a binary tree is a complete binary tree
+int BinaryTreeComplete(BTNode* root);
